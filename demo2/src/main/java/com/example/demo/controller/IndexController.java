edd45224aae92b7cb492bccb2086f050c636490f
taskfiba <taskfiba@yahoo.ne.jp>
2021-08-01 18:25:39 +0900

    コメントは必ず書いてください

---
+++ demo2/src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String index(Model m) {
+		m.addAttribute("message", "Hello Spring Boot!");
+		return "index";
+	}
+	
+}
---
+++ study/src/main/java/com/internous/study/Controller/UserController.java
@@ -0,0 +1,27 @@
+package com.internous.study.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.internous.study.model.form.UserForm;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@RequestMapping("")
+	public String userIndex() {
+		return "userindex";
+	}
+	
+	@RequestMapping(value="register" , method=RequestMethod.POST)
+	public String registerUser(
+		@ModelAttribute("userForm") UserForm form,
+		Model model) {
+	  model.addAttribute("userForm", form);
+	  return "register";
+	}
+}
